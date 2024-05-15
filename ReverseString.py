60b16c6677469913133b57658d48ee39c83e2e3b
Ahmad Husain <154722698+khanahmad-tech@users.noreply.github.com>
2024-05-15 05:02:47 -0700

    Update ReverseString.py

--- ReverseString.py
+++ ReverseString.py
@@ -3,3 +3,24 @@ def reverse_string1(s):
 
 s = "Ahmad Khan"
 print(reverse_string1(s))
+#2
+
+def reverse_string3(s):
+    reversed_str = ''
+    for char in s:
+        reversed_str = char + reversed_str
+    return reversed_str
+
+# Test
+s = "Ahmad Khan"
+print(reverse_string3(s))
+ 
+#3
+
+
+def reverse_string2(s):
+    return ''.join(reversed(s))
+
+s = "Hello, World!"
+print(reverse_string2(s))
+
